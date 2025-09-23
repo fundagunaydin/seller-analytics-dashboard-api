811e944c3583b26692407d19dbd2f46f3d30fae2
funda günaydın <fundagunaydin35@hotmail.com>
2025-09-23 16:26:18 +0300

    Add product API with EF Core, Redis, and Swagger support and Updated `ProductService.csproj` to include NuGet packages

---
+++ ProductService/Controller/ProductsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using ProductService.Data;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly AppDbContext _context;
+    private readonly IDistributedCache _cache;
+
+    public ProductsController(AppDbContext context, IDistributedCache cache)
+    {
+        _context = context;
+        _cache = cache;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var cacheKey = "products_list";
+        var cached = await _cache.GetStringAsync(cacheKey);
+
+        if (!string.IsNullOrEmpty(cached))
+            return Ok(JsonConvert.DeserializeObject<List<Product>>(cached));
+
+        var products = _context.Products.ToList();
+        await _cache.SetStringAsync("products_list", JsonConvert.SerializeObject(products));
+
+        return Ok(products);
+    }
+
+    [HttpPost]
+    public IActionResult Create(Product product)
+    {
+        _context.Products.Add(product);
+        _context.SaveChanges();
+
+        _cache.Remove("products_list");
+
+        return Ok(product);
+    }
+}
--- ProductService/Program.cs
+++ ProductService/Program.cs
@@ -1,41 +1,40 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using ProductService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Product Service v1");
+        c.RoutePrefix = string.Empty; // <-- bu satýr önemli
+    });
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthorization();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
